948398f6d829ab057dd80e76d7ce6dd0be4a6da5
manthanbelekar-design <manthan.belekar@somaiya.edu>
2026-06-30 15:03:15 +0530

    Add files via upload

---
+++ code for capturing faces.py
@@ -0,0 +1,35 @@
+import cv2
+import os
+
+name = "Manthan"
+path = f"dataset/{name}"
+
+os.makedirs(path, exist_ok=True)
+
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades +
+    "haarcascade_frontalface_default.xml"
+)
+
+cap = cv2.VideoCapture(0)
+
+count = 0
+
+while count < 30:
+
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(
+        gray,
+        scaleFactor=1.3,
+        minNeighbors=5
+    )
+
+    for (x, y, w, h) in faces:
+
+        face = gray[y:y+h, x:x+w]
